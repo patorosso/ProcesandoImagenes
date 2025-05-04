213f0189b63a34506558a9ec3b847ae339c39d5e
pato-rosso <patriciosimonrosso@gmail.com>
2025-05-04 16:42:55 -0300

    add more images, play with them

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -2,15 +2,42 @@ package algoritmos;
 
 public class Algoritmos {
 
-	public static int[][] dilatacion(int[][] imagen, int vecindario) {
-		int height = imagen.length;
-		int width = imagen[0].length;
-		
-		int[][] matrizDilatada = {{}};
-
-
-		return matrizDilatada;
-	}
+	public static int[][] dilatacion(int[][] image, int[][] mask) {
+        int rows = image.length;
+        int cols = image[0].length;
+        int maskRows = mask.length;
+        int maskCols = mask[0].length;
+ 
+        int[][] output = new int[rows][cols];
+ 
+        int rOffset = maskRows / 2;
+        int cOffset = maskCols / 2;
+ 
+        for (int i = rOffset; i < rows - rOffset; i++) {
+            for (int j = cOffset; j < cols - cOffset; j++) {
+                boolean found = false;
+ 
+                // Recorre el vecindario según la máscara
+                for (int mi = 0; mi < maskRows; mi++) {
+                    for (int mj = 0; mj < maskCols; mj++) {
+                        if (mask[mi][mj] == 1) {
+                            int ii = i + mi - rOffset;
+                            int jj = j + mj - cOffset;
+                            if (image[ii][jj] == 0) {
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (found) break;
+                }
+ 
+                output[i][j] = found ? 0 : 255;
+            }
+        }
+ 
+        return output;
+    }
 	
 	public static int[][] erosion(int[][] imagen, int vecindario) {
 		int height = imagen.length;
--- src/main/Main.java
+++ src/main/Main.java
@@ -11,18 +11,18 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			int[][] imagen = null;
-			imagen = Archivo.LeerArchivo("./resources/in/binary_figures.pgm");
+			imagen = Archivo.LeerArchivo("./resources/in/binary_flower.pgm");
 
 			System.out.println("Imagen previa:\n");
 			System.out.println(Matriz.ToString(imagen));
 			System.out.println("\n\n----------------------------------\n\n");
 			
-			int vecindad = 3;
+			int[][] vecindad = {{1,1,1},{1,1,1},{1,1,1}};
 			int[][] imagenDilatada = Algoritmos.dilatacion(imagen, vecindad);
 
 			System.out.println("Imagen post algoritmo:\n");
 			System.out.println(Matriz.ToString(imagenDilatada));
-			Archivo.GuardarPGM_P2(imagenDilatada, "./resources/out/cat2.pgm", 255);
+			Archivo.GuardarPGM_P2(imagenDilatada, "./resources/out/binary_flower_converted_inverted_2.pgm", 255);
 
 		} catch (IOException e) {
 			// TODO Bloque catch generado automaticamente
