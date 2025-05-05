a2572d36d0d7c778176d614e35f1874918c0cf3e
pato-rosso <patriciosimonrosso@gmail.com>
2025-05-05 20:52:53 -0300

    add tests for dilatacion

--- test/algoritmos/DilatacionTests.java
+++ test/algoritmos/DilatacionTests.java
@@ -1,29 +1,148 @@
-package escalamientoNearestNeighbour;
-
-import java.io.IOException;
+package algoritmos;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import archivo.Archivo;
+import algoritmos.Algoritmos;
 
 public class DilatacionTests {
 
-	@Test
-	public void dilatar() throws IOException { // manteniendo las proporciones
-		// Arrange
-		int[][] imagen = Archivo.LeerArchivo("./resources/in/tablero blanco negro 3x3.pgm");
-		int[][] esperado = { { 0, 0, 0, 255, 255, 255, 0, 0, 0 }, { 0, 0, 0, 255, 255, 255, 0, 0, 0 },
-				{ 0, 0, 0, 255, 255, 255, 0, 0, 0 }, { 255, 255, 255, 0, 0, 0, 255, 255, 255 },
-				{ 255, 255, 255, 0, 0, 0, 255, 255, 255 }, { 255, 255, 255, 0, 0, 0, 255, 255, 255 },
-				{ 0, 0, 0, 255, 255, 255, 0, 0, 0 }, { 0, 0, 0, 255, 255, 255, 0, 0, 0 },
-				{ 0, 0, 0, 255, 255, 255, 0, 0, 0 } };
+	 	@Test
+	    public void testDilatacionSimple() {
+	        int[][] imagen = {
+	            {0, 0, 0},
+	            {0, 255, 0},
+	            {0, 0, 0}
+	        };
 
-		// Act
-		int[][] imagenEscalada = EscalamientoNearestNeighbour.nearestNeighbourEscalado(imagen, 9, 9);
+	        int[][] vecindad = {
+	            {1, 1, 1},
+	            {1, 1, 1},
+	            {1, 1, 1}
+	        };
 
-		// Assert
-		Assert.assertArrayEquals(esperado, imagenEscalada);
-	}
+	        int[][] esperado = {
+	            {255, 255, 255},
+	            {255, 255, 255},
+	            {255, 255, 255}
+	        };
+
+	        int[][] resultado = Algoritmos.dilatacion(imagen, vecindad);
+
+	        for (int i = 0; i < esperado.length; i++) {
+	            Assert.assertArrayEquals("Error en fila " + i, esperado[i], resultado[i]);
+	        }
+	    }
+	 
+	 @Test
+	 public void testDilatacionSinBlancos() {
+	     int[][] imagen = {
+	         {0, 0, 0},
+	         {0, 0, 0},
+	         {0, 0, 0}
+	     };
+
+	     int[][] vecindad = {
+	         {1, 1, 1},
+	         {1, 1, 1},
+	         {1, 1, 1}
+	     };
+
+	     int[][] esperado = {
+	         {0, 0, 0},
+	         {0, 0, 0},
+	         {0, 0, 0}
+	     };
+
+	     int[][] resultado = Algoritmos.dilatacion(imagen, vecindad);
+
+	     for (int i = 0; i < esperado.length; i++) {
+	         Assert.assertArrayEquals("Error en fila " + i, esperado[i], resultado[i]);
+	     }
+	 }
+	 
+	 
+	 @Test
+	 public void testDilatacionMultiplesBlancos() {
+	     int[][] imagen = {
+	         {255, 0, 0},
+	         {0, 0, 0},
+	         {0, 0, 255}
+	     };
+
+	     int[][] vecindad = {
+	         {1, 1, 1},
+	         {1, 1, 1},
+	         {1, 1, 1}
+	     };
+
+	     int[][] esperado = {
+	         {255, 255, 0},
+	         {255, 255, 255},
+	         {0, 255, 255}
+	     };
+
+	     int[][] resultado = Algoritmos.dilatacion(imagen, vecindad);
+
+	     for (int i = 0; i < esperado.length; i++) {
+	         Assert.assertArrayEquals("Error en fila " + i, esperado[i], resultado[i]);
+	     }
+	 }
+	 
+	 
+	 @Test
+	 public void testDilatacionConVecindadCruz() {
+	     int[][] imagen = {
+	         {0, 0, 0},
+	         {0, 255, 0},
+	         {0, 0, 0}
+	     };
+
+	     int[][] vecindad = {
+	         {0, 1, 0},
+	         {1, 1, 1},
+	         {0, 1, 0}
+	     };
+
+	     int[][] esperado = {
+	         {0, 255, 0},
+	         {255, 255, 255},
+	         {0, 255, 0}
+	     };
+
+	     int[][] resultado = Algoritmos.dilatacion(imagen, vecindad);
+
+	     for (int i = 0; i < esperado.length; i++) {
+	         Assert.assertArrayEquals("Error en fila " + i, esperado[i], resultado[i]);
+	     }
+	 }
+	 
+	 
+	 @Test
+	 public void testDilatacionEnEsquina() {
+	     int[][] imagen = {
+	         {255, 0, 0},
+	         {0, 0, 0},
+	         {0, 0, 0}
+	     };
+
+	     int[][] vecindad = {
+	         {1, 1, 1},
+	         {1, 1, 1},
+	         {1, 1, 1}
+	     };
+
+	     int[][] esperado = {
+	         {255, 255, 0},
+	         {255, 255, 0},
+	         {0, 0, 0}
+	     };
+
+	     int[][] resultado = Algoritmos.dilatacion(imagen, vecindad);
+
+	     for (int i = 0; i < esperado.length; i++) {
+	         Assert.assertArrayEquals("Error en fila " + i, esperado[i], resultado[i]);
+	     }
+	 }
 
 }
