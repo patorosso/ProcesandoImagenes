a4628e948f18507b53487fcd1c0dd13e41a12e4c
pato-rosso <patriciosimonrosso@gmail.com>
2025-05-05 20:52:45 -0300

    rework dilatacion algorithm and add erosion one

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -2,51 +2,93 @@ package algoritmos;
 
 public class Algoritmos {
 
-	public static int[][] dilatacion(int[][] image, int[][] mask) {
-        int rows = image.length;
-        int cols = image[0].length;
-        int maskRows = mask.length;
-        int maskCols = mask[0].length;
- 
-        int[][] output = new int[rows][cols];
- 
-        int rOffset = maskRows / 2;
-        int cOffset = maskCols / 2;
- 
-        for (int i = rOffset; i < rows - rOffset; i++) {
-            for (int j = cOffset; j < cols - cOffset; j++) {
-                boolean found = false;
- 
-                // Recorre el vecindario según la máscara
-                for (int mi = 0; mi < maskRows; mi++) {
-                    for (int mj = 0; mj < maskCols; mj++) {
-                        if (mask[mi][mj] == 1) {
-                            int ii = i + mi - rOffset;
-                            int jj = j + mj - cOffset;
-                            if (image[ii][jj] == 0) {
-                                found = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (found) break;
-                }
- 
-                output[i][j] = found ? 0 : 255;
-            }
-        }
- 
-        return output;
-    }
-	
-	public static int[][] erosion(int[][] imagen, int vecindario) {
-		int height = imagen.length;
-		int width = imagen[0].length;
-		
-		int[][] matrizDilatada = {{}};
+	public static final int NEGRO = 0;
+	public static final int BLANCO = 255;
+	public static final int BIT_A_IGNORAR = 0;
 
+	public static int[][] dilatacion(int[][] imagen, int[][] vecindad) {
+		int filas = imagen.length;
+		int columnas = imagen[0].length;
+		int filasVecindad = vecindad.length;
+		int columnasVecindad = vecindad[0].length;
 
-		return matrizDilatada;
+		int[][] resultado = new int[filas][columnas];
+
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+
+				boolean aplicarDilatacion = false;
+
+				for (int vi = 0; vi < filasVecindad; vi++) {
+					for (int vj = 0; vj < columnasVecindad; vj++) {
+
+						if (vecindad[vi][vj] == BIT_A_IGNORAR)
+							continue;
+
+						int ni = i + vi - vecindad.length / 2;
+						int nj = j + vj - vecindad[0].length / 2;
+
+						if (ni >= 0 && ni < filas && nj >= 0 && nj < columnas) {
+							if (imagen[ni][nj] == BLANCO) {
+								aplicarDilatacion = true;
+								break;
+							}
+						} // else: Fuera del rango => ignoramos
+					}
+
+					if (aplicarDilatacion)
+						break;
+				}
+
+				resultado[i][j] = aplicarDilatacion ? BLANCO : NEGRO;
+			}
+		}
+
+		return resultado;
+	}
+
+	public static int[][] erosion(int[][] imagen, int[][] vecindad) {
+		int filas = imagen.length;
+		int columnas = imagen[0].length;
+		int filasVecindad = vecindad.length;
+		int columnasVecindad = vecindad[0].length;
+
+		int[][] resultado = new int[filas][columnas];
+
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+
+				boolean mantenerBlanco = true;
+
+				for (int vi = 0; vi < filasVecindad; vi++) {
+					for (int vj = 0; vj < columnasVecindad; vj++) {
+
+						if (vecindad[vi][vj] == BIT_A_IGNORAR)
+							continue;
+
+						int ni = i + vi - filasVecindad / 2;
+						int nj = j + vj - columnasVecindad / 2;
+
+						if (ni >= 0 && ni < filas && nj >= 0 && nj < columnas) {
+							if (imagen[ni][nj] == NEGRO) {
+								mantenerBlanco = false;
+								break;
+							}
+						} else {
+							// Fuera de los límites de la imagen => tratamos como negro
+							mantenerBlanco = false;
+							break;
+						}
+					}
+					if (!mantenerBlanco)
+						break;
+				}
+
+				resultado[i][j] = mantenerBlanco ? BLANCO : NEGRO;
+			}
+		}
+
+		return resultado;
 	}
 
 }
